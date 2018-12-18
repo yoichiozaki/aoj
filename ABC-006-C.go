a3a89e8b1f40e5c99643ead3056d55936c4ed7d1
zakimal <oza777you@gmail.com>
2018-12-19 01:06:22 +0900

    ABC-006-C

--- ABC-006-C.go
+++ ABC-006-C.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var scanner = bufio.NewScanner(os.Stdin)
+
+func main() {
+	scanner.Scan()
+	input := strings.Split(scanner.Text(), " ")
+	n, _ := strconv.Atoi(input[0])
+	m, _ := strconv.Atoi(input[1])
+	baby, man, old := 0, 0, 0
+	q := m / n // 一個体あたりの平均足本数
+	r := m % n
+	if q < 2 || 4 < q || (q == 4 && 0 < r) {
+		baby, man, old = -1, -1 ,-1
+	} else {
+		if q == 2 {
+			man = n - r
+			old = r
+			baby = 0
+		} else if q == 3 {
+			man = 0
+			old = n - r
+			baby = r
+		} else {
+			man, old = 0, 0
+			baby = n
+		}
+	}
+	fmt.Println(man, old, baby)
+}
