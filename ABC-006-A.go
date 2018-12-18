d588a50c3801ace502b0ea8660c8e303ae493dce
zakimal <oza777you@gmail.com>
2018-12-19 00:52:54 +0900

    ABC-006-ABC

--- ABC-006-A.go
+++ ABC-006-A.go
@@ -5,41 +5,17 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 var scanner = bufio.NewScanner(os.Stdin)
-const DIV = 10007
 
 func main() {
 	scanner.Scan()
 	n, _ := strconv.Atoi(scanner.Text())
-	// fmt.Println(toribocchi(n))
-	a := make([]int, n)
-	switch n {
-	case 1, 2:
-		fmt.Println(0)
-		return
-	case 3:
-		fmt.Println(1)
-		return
-	default:
-		a[0], a[1], a[2] = 0, 0, 1
-		for i := 3; i < n; i++ {
-			a[i] = (a[i-1] + a[i-2] + a[i-3])%DIV
-		}
-		fmt.Println(a[n-1]%DIV)
+	if n%3 == 0 || strings.Contains(scanner.Text(), "3") {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
 	}
 }
-
-// func toribocchi(n int) int {
-// 	switch n {
-// 	case 1:
-// 		return 0
-// 	case 2:
-// 		return 0
-// 	case 3:
-// 		return 1
-// 	default:
-// 		return (toribocchi(n-1) + toribocchi(n-2) + toribocchi(n-3))%DIV
-// 	}
-// }
